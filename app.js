b761386a621ca35d37089d4c6b5fdabcab7bd6a2
Elvis Yuan <elvisxb@gmail.com>
2019-07-15 02:16:57 -0400

    add models and webpack

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 const db = require("./config/keys").mongoURI;
+const bodyParser = require("body-parser");
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => res.send("Hello Friends"));
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
